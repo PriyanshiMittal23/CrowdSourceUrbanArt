eda0ebd42ac47c5f3a0fea25899393c88c0f3233
PriyanshiMittal23 <106468181+PriyanshiMittal23@users.noreply.github.com>
2024-07-06 15:41:35 +0530

    initial setup

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import Card from '../Components/Card'
 
 const Home = () => {
   return (
     <div>
+      
       <Navbar/>
+      <Card/>
       home
     </div>
   )
